8f2c3dfc285bf8fe41c711c25f79f5c7f6bf31fa
khuship1409 <khushipatel140904@gmail.com>
2025-09-12 08:01:10 +0530

    initial commit

---
+++ User.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class User {
+    /*these variables are intialized in all the classes using constructor*/
+    private Connection connection;
+    private Scanner scanner;
+
+    public User(Connection connection,Scanner scanner){
+        this.connection=connection;
+        this.scanner=scanner;
+    }
+
+    /* this method is for registration of the user */
+    public void register(){
+        scanner.nextLine();
+        System.out.print("fullname: ");
+        String full_name= scanner.nextLine();
+        System.out.print("email: ");
+        String email= scanner.nextLine();
+        System.out.print("password: ");
+        String password= scanner.nextLine();
+
+        /*check whether the user already exists */
+        if(user_exists(email)){
+            System.out.println("user already exists for this email id!!!");
+            return;
+        }
+        /* query for the insertion of the user data in the db */
+        String registerQuery="insert into user(full_name, email, password) values(?,?,?)";
+
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(registerQuery);
+            preparedStatement.setString(1,full_name);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3,password);
+
+            int affectedRows=preparedStatement.executeUpdate();
+
+            /*checks if all the entries are filled by the user */
+
+            if(affectedRows>0){
+                System.out.println("registration completed!!");
+            }
+            else{
+                System.out.println("registration failed");
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /* this is the method for the login of the user */
+
+    public String login(){
+        scanner.nextLine();
+        System.out.print("email: ");
+        String email=scanner.nextLine();
+        System.out.print("password: ");
+        String password=scanner.nextLine();
+
+        String login_query = "select * from user where email=? and password=?";
+        try{
+        PreparedStatement preparedStatement=connection.prepareStatement(login_query);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                return email;
+            }
+            else{
+                return null;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    return null;
+    }
+
+    /* this method tells us whether the user already exists or not */
+
+    public boolean user_exists(String email){
+        String query = "select * from user where email=?";
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(query);
+            preparedStatement.setString(1,email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+             if(resultSet.next()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
